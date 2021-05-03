c7eac1a3729e5f06eb04643a9b67845fbe5962fc
Kevin Nguyen <Airakunnn@gmail.com>
2021-05-03 15:56:34 -0700

    Delete ScabbleWordFinder-main directory

--- ScabbleWordFinder-main/src/index.js
+++
@@ -1,6 +0,0 @@
-var letterInput = document.getElementById('letters');
-var foundWords = document.getElementById('words');
-
-var findWords = function() {
-  foundWords.innerHTML = ScrabbleWordFinder.find(letterInput.value.toLowerCase()).join('\n');
-};
